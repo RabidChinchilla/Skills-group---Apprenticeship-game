70d0e0a80f51229915aaa29956f4328fc262006f
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-03-18 20:47:13 +0000

    Set resolution and set up buttons
    set the resolution (1920x1080) so it will be the same on all screens and set up each button so the game flows as it should

---
+++ Skills group - Apprenticeship game/Assets/Scripts/displayButtonsOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayButtonsOnClick : MonoBehaviour {
+
+    public GameObject brickLayingBtn;
+    public GameObject pipeLayingBtn;
+    private bool active = false;
+
+	public void displayButtons()
+    {
+        if (active == false)
+        {
+            brickLayingBtn.SetActive(true);
+            pipeLayingBtn.SetActive(true);
+            active = true;
+        }
+        else
+        {
+            brickLayingBtn.SetActive(false);
+            pipeLayingBtn.SetActive(false);
+            active = false;
+        }
+    }
+}
