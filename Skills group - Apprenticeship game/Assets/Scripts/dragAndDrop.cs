4a094e902a48e98619ac01e18fe22f28c416c092
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-03-25 16:59:58 +0000

    worked on brick laying puzzle game
    added some art and a prototype puzzle to the scene and started scripting the drag and drop system and checking to see if the correct puzzle piece has been dropped

---
+++ Skills group - Apprenticeship game/Assets/Scripts/dragAndDrop.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragAndDrop : MonoBehaviour
+{
+    private bool draggingItem = false;
+    private GameObject draggedObject;
+    private Vector2 touchOffset;
+
+    void Update()
+    {
+        if (HasInput)
+        {
+            DragOrPickUp();
+        }
+        else
+        {
+            if (draggingItem)
+                DropItem();
+        }
+    }
+
+    Vector2 CurrentTouchPosition
+    {
+        get
+        {
+            //takes position of the mouse based on where it is in the cameras view
+            Vector2 inputPos;
+            inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return inputPos;
+        }
+    }
+
+    private void DragOrPickUp()
+    {
+        var inputPosition = CurrentTouchPosition;
+
+        if (draggingItem)
+        {
+            //move object position using the input and the offset
+            draggedObject.transform.position = inputPosition + touchOffset;
+        }
+        else
+        {
+            RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 0.5f);
+            if (touches.Length > 0)
+            {
+                var hit = touches[0];
+                if (hit.transform != null)
+                {
+                    draggingItem = true;
+                    draggedObject = hit.transform.gameObject;
+                    touchOffset = (Vector2)hit.transform.position - inputPosition;
+                    draggedObject.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+                }
+            }
+        }
+    }
+
+    private bool HasInput
+    {
+        get
+        {
+            // returns true if either the mouse button is down or at least one touch is felt on the screen
+            return Input.GetMouseButton(0);
+        }
+    }
+
+    void DropItem()
+    {
+        //stops the object from being dragged and resets the scale
+        draggingItem = false;
+        draggedObject.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+    }
+}
---
+++ Skills group - Apprenticeship game/Assets/Scripts/puzzlePieceCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzlePieceCheck : MonoBehaviour
+{
+    public string tag;
+    public GameObject correctPiece = null;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "brick1")
+        {
+            //collision.gameObject.SetActive(false);
+            //Instantiate(correctPiece);
+            collision.gameObject.transform.position = this.transform.position;
+        }
+    }
+}
