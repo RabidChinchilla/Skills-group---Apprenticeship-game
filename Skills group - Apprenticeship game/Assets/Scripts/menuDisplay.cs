9ba703574b550bf096453308cc6c2b614b0c3191
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-03-25 14:29:40 +0000

    added menu button and more art
    added a menu button to the map screen so that when clicked it disabled all other buttons and gave the player the option to change the option settings or quit back to the main menu

--- Skills group - Apprenticeship game/Assets/Scripts/menuDisplay.cs
+++ Skills group - Apprenticeship game/Assets/Scripts/menuDisplay.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class displayButtonsOnClick : MonoBehaviour {
-
+public class menuDisplay : MonoBehaviour
+{
     public GameObject buttonOne;
     public GameObject buttonTwo;
+    public GameObject schoolBtn = null;
+    public GameObject workBtn = null;
+    public GameObject shopsBtn = null;
     private bool active = false;
 
-	public void displayButtons()
+    public void displayButtons()
     {
         if (active == false)
         {
             buttonOne.SetActive(true);
             buttonTwo.SetActive(true);
+            schoolBtn.SetActive(false);
+            workBtn.SetActive(false);
+            shopsBtn.SetActive(false);
             active = true;
         }
         else
         {
             buttonOne.SetActive(false);
             buttonTwo.SetActive(false);
+            schoolBtn.SetActive(true);
+            workBtn.SetActive(true);
+            shopsBtn.SetActive(true);
             active = false;
         }
     }
