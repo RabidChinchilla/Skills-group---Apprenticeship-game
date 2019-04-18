20a21cc00827885c99317b5922f79d05cabe780a
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-04-18 20:20:07 +0100

    fixed randomNum.Next() error
    had an error that said .Next() wasn't able to be used and fixed it by adding using System in the script

--- Skills group - Apprenticeship game/Assets/Scripts/randomPattern.cs
+++ Skills group - Apprenticeship game/Assets/Scripts/randomPattern.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,11 +9,15 @@ public class randomPattern : MonoBehaviour
     public GameObject button2;
     public GameObject button3;
     public GameObject button4;
+    public GameObject postion1;
+    public GameObject postion2;
+    public GameObject postion3;
+    public GameObject postion4;
 
     // Start is called before the first frame update
     void Start()
     {
-        Random randomNum = new Random();
+        System.Random randomNum = new System.Random();
         int number = randomNum.Next(0, 4);
     }
 
