a9885b40e43e1298074bf0cdcca4bd5fba5e2904
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-04-18 15:46:18 +0100

    started coding script for education mini-game

---
+++ Skills group - Apprenticeship game/Assets/Scripts/randomPattern.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomPattern : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random randomNum = new Random();
+        int number = randomNum.Next(0, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
