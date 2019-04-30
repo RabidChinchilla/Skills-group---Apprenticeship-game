2b15065d5e0617acd2fed162584cc0d0f3e42e28
Ryan Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2019-04-30 16:53:49 +0100

    Main art assets
    main art assets added, awaiting obstacle assets

--- Skills group - Apprenticeship game/Assets/Scripts/pipePlacement.cs
+++ Skills group - Apprenticeship game/Assets/Scripts/pipePlacement.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pipePlacement : MonoBehaviour
-{
+public class pipePlacement : MonoBehaviour {
+
+    [SerializeField]
+    private string correctObjectTag = " ";
+    [SerializeField]
+    private GameObject correctObject;
+
+    //private bool dragging;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //dragging = dragAndDrop.
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision!");
+        if(collision.gameObject.tag == correctObjectTag)
+        {
+            Debug.Log("Can place");
+            correctObject.SetActive(true);
+            collision.gameObject.SetActive(false);
+
+        }
     }
 }
