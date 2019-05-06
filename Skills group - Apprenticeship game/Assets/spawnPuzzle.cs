a5682ec17726dcea3e44c4b266cba6134f566d61
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-05-06 17:35:38 +0100

    added puzzle generator and win condition

---
+++ Skills group - Apprenticeship game/Assets/spawnPuzzle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPuzzle : MonoBehaviour
+{
+    public GameObject puzzle1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(puzzle1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
