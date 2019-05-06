57954ed347773a31ad81006104741e17c1850c03
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-05-06 17:15:03 +0100

    brick laying game progress
    came up with a system to give infinite bricks

---
+++ Skills group - Apprenticeship game/Assets/Scripts/spawnNewBrick.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class spawnNewBrick : MonoBehaviour
+{
+    public string brickTag;
+    private bool brickNeeded = false;
+    public GameObject pieceNeeded;
+    private Vector3 position;
+    private Vector3 piecePosition;
+    private Quaternion rotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.transform.position;
+        rotation = this.transform.rotation;
+        piecePosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (brickNeeded == true)
+        {
+            Instantiate(pieceNeeded, position, rotation);
+            brickNeeded = false;
+        }
+
+        if (this.transform.position != piecePosition)
+        {
+            this.transform.position = piecePosition;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        brickNeeded = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        brickNeeded = true;
+    }
+}
