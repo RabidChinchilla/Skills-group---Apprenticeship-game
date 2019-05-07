19c11dfe65a17270a35f7d2fa69b082b1bca364d
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-05-07 01:27:20 +0100

    created 2nd puzzle

--- Skills group - Apprenticeship game/Assets/spawnPuzzle.cs
+++ Skills group - Apprenticeship game/Assets/spawnPuzzle.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class spawnPuzzle : MonoBehaviour
 {
     public GameObject puzzle1;
+    public GameObject puzzle2;
+    private float puzzleSelection;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(puzzle1);
+        puzzleSelection = Random.Range(0.0f, 1.0f);
+
+        if (puzzleSelection < 0.5f)
+        {
+            Instantiate(puzzle1);
+        }
+        else
+        {
+            Instantiate(puzzle2);
+        }
     }
 
     // Update is called once per frame
