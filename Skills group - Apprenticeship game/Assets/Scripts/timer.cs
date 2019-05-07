51a7d9805ba295482e02c85bc7ee91b3ce618d04
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-05-07 01:50:52 +0100

    completed making timer

--- Skills group - Apprenticeship game/Assets/Scripts/timer.cs
+++ Skills group - Apprenticeship game/Assets/Scripts/timer.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timeLeft = 30.0f; //set 30 seconds
+    //public bool stop = false;
 
-    // Update is called once per frame
+    private float minutes;
+    private float seconds;
+
+    public Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+    }
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        text.text = "time Left: " + Mathf.Round(timeLeft); //round so timer looks neat
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene("fail");
+        }
     }
 }
