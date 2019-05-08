e8db88caf7805b5031177a2b3d031d8fe639974a
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-05-08 02:55:22 +0100

    made it so pattern to copy is destroyed after time

---
+++ Skills group - Apprenticeship game/Assets/Scripts/destroyAfterTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyAfterTime : MonoBehaviour
+{
+    public float timeLeft = 3.0f; //set 3 seconds
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
